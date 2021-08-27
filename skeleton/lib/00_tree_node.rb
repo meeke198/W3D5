da2d80147311f1eecccdd7405f0f0a06e8ed843a
Woodstock <victorhe97@gmail.com>
2021-08-27 15:29:04 -0400

    Completed Polytree.

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,57 @@
 class PolyTreeNode
 
+  attr_reader :value, :children, :parent
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent_node)
+    unless self.parent.nil?
+      self.parent.children.delete(self)
+    end
+
+    @parent = new_parent_node
+    unless new_parent_node.nil?
+      new_parent_node.children << self
+    end
+  end
+
+    # is the node's parent the self.parent
+    # passed_node is the a node isntance parent
+    # should add the child node to the passed node's children
+
+    # [] parent node (passed_node)
+    #   \
+    #     []  (child_node)
+
+    # when reassigning
+    # passed_node.children << node (self)
+    # remove the node (self) from old parent's children
+    # node should only have one parent
+      # remove self from "old parent"
+
+    # [ ____ , _____, ______, self ] old_parent.pop
+
+  def add_child(child_node)
+    # self == parent_node
+    child_node.parent = self # child_node.parent=(self)
+  end
+
+  def remove_child(child_node)
+    raise 'error' if child_node.parent.nil?
+    child_node.parent = nil
+  end
+
+  def inspect
+    {value: @value,
+      children: @children.map { |child| child.value }
+    }.inspect
+  end
+
 end
+
+
+
